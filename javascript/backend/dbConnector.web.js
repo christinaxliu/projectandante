db6026e964507fd98e1e497c601faed742f20038
christinaxliu <christinaliu2026@gmail.com>
2024-12-27 12:36:02 -0800

    Add backend web module function to fetch row from MySQL db that ssatisfy condition and order by a specified column in ascending order.

--- javascript/backend/dbConnector.web.js
+++ javascript/backend/dbConnector.web.js
@@ -64,3 +64,29 @@ export const fetchRowsByConditionFromDb = webMethod(
         );
     }
 );
+
+// Backend web module function to fetch rows that satisfy condition from MySQL (Google Cloud SQL) database and
+// sort the results in the ascending order on the column specified by the orderByColumnName input parameter.
+// The condition is specified by the input parameters: conditionColumnName and conditionColumnValue. The rows
+// that satisfy the following conditions will be returned:
+//   column_name == column_value
+export const fetchRowsByConditionAndSortAscendingFromDb = webMethod(
+    Permissions.Anyone, 
+    (dbCollectionTableName, conditionColumnName, conditionColumnValue, orderByColumnName) => { 
+        let options = {
+          "suppressAuth": true
+        };
+
+        return wixData.query(dbCollectionTableName)
+            .eq(conditionColumnName, conditionColumnValue)
+            .ascending(orderByColumnName)
+            .find(options)
+            .then((results) => {
+                return results;
+            })
+            .catch((error) => {
+                return 0;
+            }
+        );
+    }
+);
