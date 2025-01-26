945899bb01ed6bd66f64e4dd4915249625b650cb
christinaxliu <christinaliu2026@gmail.com>
2025-01-25 19:33:55 -0800

    Update the javascript code to populate leaders info

--- javascript/web/populatingLeadershipInfo.js
+++ javascript/web/populatingLeadershipInfo.js
@@ -2,49 +2,62 @@
 import { fetchRowsByConditionFromDb } from "backend/dbConnector.web"
 import { fetchRowsByConditionAndSortAscendingFromDb } from "backend/dbConnector.web"
 
-$w.onReady(async function () {
-    const profilePicElemPrefix = "#leaderPic";
-    const nameElemPrefix = "#leaderName";
-    const positionElemPrefix = "#leaderPosition";
-    const bioElemPrefix = "#leaderBio";
-    const mobileElemPostfix = "-mobile";
+const profilePicElemPrefix = "#leaderPic";
+const nameElemPrefix = "#leaderName";
+const positionElemPrefix = "#leaderPosition";
+const bioElemPrefix = "#leaderBio";
+const mobileElemPostfix = "-Mobile";
 
-    // Call backend web module to fetch co-founders from MySQL (Google Cloud SQL) database
-    var cofounderResults = await fetchRowsByConditionFromDb("mysqlConnections/leaders",
-                                                            "position_level",
-                                                            "Co-Founder");
-    var coFounders = cofounderResults.items;
-    var numCoFounders = coFounders.length;
-
-    // Update co-founders's information
-    for (let i = 0; i < coFounders.length; i++) {
-        $w(profilePicElemPrefix + i).src = coFounders[i].profile_pic_uri;
-        $w(nameElemPrefix + i).text = coFounders[i].first_name + " " + coFounders[i].last_name;
-        $w(positionElemPrefix + i).text = coFounders[i].position;
-        $w(bioElemPrefix + i).text = coFounders[i].bio;
-        let elemMobileBio = $w(bioElemPrefix + i + mobileElemPostfix);
+function populateLeadersInfo(leaderResultsFromDb, numLeadersWithInfoPopulated) {
+    // Update leaders' information
+    // There are numPreLeaders before this group of leaders, so the leader index in the loop to locate the
+    // corresponding elements is: numPreLeaders + i
+    var leaders = leaderResultsFromDb.items;
+    for (let i = 0; i < leaders.length; i++) {
+        let leaderElemIndex = numLeadersWithInfoPopulated + i;
+        $w(profilePicElemPrefix + leaderElemIndex).src = leaders[i].profile_pic_uri;
+        $w(nameElemPrefix + leaderElemIndex).text = leaders[i].first_name + " " + leaders[i].last_name;
+        $w(positionElemPrefix + leaderElemIndex).text = leaders[i].position;
+        $w(bioElemPrefix + leaderElemIndex).text = leaders[i].bio;
+        let elemMobileBio = $w(bioElemPrefix + leaderElemIndex + mobileElemPostfix);
         if (elemMobileBio && elemMobileBio != undefined && elemMobileBio.length != 0) {
-            elemMobileBio.text = coFounders[i].bio;
+            elemMobileBio.text = leaders[i].bio;
         }
     }
+}
+
+$w.onReady(async function () {
+    var numLeadersWithInfoPopulated = 0;
+
+    // Call backend web module to fetch co-founders from MySQL (Google Cloud SQL) database
+    var cofounderResults = await fetchRowsByConditionFromDb(
+        "mysqlConnections/leaders",
+        "position_level",
+        "Co-Founder");
+
+    // Populate co-founders' information
+    populateLeadersInfo(cofounderResults, numLeadersWithInfoPopulated);
+    numLeadersWithInfoPopulated += cofounderResults.items.length;
 
     // Call backend web module to fetch directors from MySQL (Google Cloud SQL) database
-    var directorResults = await fetchRowsByConditionAndSortAscendingFromDb("mysqlConnections/leaders",
-	                                                                   "position_level",
-                                                                           "Director",
-                                                                           "sequence_in_postition_level");
-    var directors = directorResults.items;
-    var numDirectors = directors.length;
+    var directorResults = await fetchRowsByConditionAndSortAscendingFromDb(
+        "mysqlConnections/leaders",
+        "position_level",
+        "Director",
+        "sequence_in_postition_level");
 
-    // Update directors's information
-    // There are numCoFounders before the directors, so the director index in the loop to locate the corresponding
-    // elements is: numCoFounders + i
-    for (let i = 0; i < directors.length; i++) {
-        let leaderElemIndex = numCoFounders + i;
-        $w(profilePicElemPrefix + leaderElemIndex).src = directors[i].profile_pic_uri;
-        $w(nameElemPrefix + leaderElemIndex).text = directors[i].first_name + " " + directors[i].last_name;
-        $w(positionElemPrefix + leaderElemIndex).text = directors[i].position;
-        $w(bioElemPrefix + leaderElemIndex).text = directors[i].bio;
-    }
+    // Populate directors' information
+    populateLeadersInfo(directorResults, numLeadersWithInfoPopulated);
+    numLeadersWithInfoPopulated += directorResults.items.length;
+
+    // Call backend web module to fetch committee from MySQL (Google Cloud SQL) database
+    var committeeResults = await fetchRowsByConditionAndSortAscendingFromDb(
+        "mysqlConnections/leaders",
+        "position_level",
+        "Committee",
+        "sequence_in_postition_level");
+
+    // Populate committee's information
+    populateLeadersInfo(committeeResults, numLeadersWithInfoPopulated);
+    numLeadersWithInfoPopulated += committeeResults.items.length;
 });
-
